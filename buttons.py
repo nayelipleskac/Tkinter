3f2d48ceb5551de3cd8680a51900ffe02b64d6a1
Nayeli Pleskac <pleskac510@gmail.com>
2022-01-30 12:12:09 -0800

    random password generator

--- buttons.py
+++ buttons.py
@@ -70,15 +70,109 @@ root.title('Buttons')
 
 # root.mainloop()
 
-def submit():
-    print('submit!!')
-    a = radio_variable.get()
-    b = current_val.get()
-    print('radio choice ', a, 'password length ', b)
-    if a == 0: #low
-        #random alphabets
-        pass
+# import random, string
+# password = ''
+# def submit():
+#     global password
+#     a = radio_variable.get()
+#     b = current_val.get()
+#     letters = string.ascii_lowercase
+#     numbers = string.digits
+#     special_chr = string.punctuation
+#     print('radio choice ', a, 'password length ', b)
+#     if a == 0: #low
+#         low_password = ''.join(random.choice(letters) for i in range(b))
+#         print(low_password)
+#         password = low_password
+#         label2['text'] = low_password
+#         root.update()
         
+#     if a == 1: #mid
+#         mid_password = ''.join(random.choice(letters + numbers) for i in range(b))
+#         print(mid_password)
+#         password = mid_password
+#         label2['text'] = mid_password
+
+
+#     if a == 2: #high
+#         high_password = ''.join(random.choice(letters + numbers + special_chr) for i in range(b))
+#         print(high_password)
+#         password = high_password
+#         label2['text'] = high_password
+
+    
+# root.geometry('300x200')
+# frame1 = Frame(root, width = 400, height = 50)
+# frame1.pack(fill = BOTH)
+
+# frame2 = Frame(root, width = 200, height = 50)
+# frame2.pack(fill = BOTH)
+# frame3 = Frame(root, width = 200, height = 50)
+# frame3.pack(fill = BOTH)
+# frame4 = Frame(root, width = 200, height = 50)
+# frame4.pack(fill = BOTH)
+
+# # password = 
+# label1 = Label(frame1, text= 'Generated Password: ',fg = 'blue').grid(row = 1, column = 1)
+# label2 = Label(frame1, textvariable = password, fg = 'black')
+# label2.grid(row = 1, column = 2)
+
+# label3 = Label(frame2, text= 'Password Strength: ')
+# label3.grid(row = 2, column = 1)
+# #three radios
+# radio_variable = IntVar()
+# radio_low = Radiobutton(frame2, text = 'Low', variable = radio_variable, value= 0)
+# radio_low.grid(row =2, column = 2)
+# radio_mid = Radiobutton(frame2, text = 'Medium', variable = radio_variable, value= 1)
+# radio_mid.grid(row = 3, column = 2)
+# radio_high = Radiobutton(frame2, text = 'High', variable = radio_variable, value= 2)
+# radio_high.grid(row = 4, column = 2)
+
+# label4 = Label(frame3, text= 'Password Length: ')
+# label4.grid(row = 5, column = 1)
+# current_val = IntVar()
+# password_length = Spinbox(frame3, from_ = 6, to= 12, textvariable = current_val).grid(row = 5, column = 2)
+# submit_bttn = Button(frame4, text = 'Submit', command = submit, bg = 'green', fg = 'white').grid(row = 6, column = 2)
+
+# root.mainloop()
+
+
+#login-app
+password = ''
+
+def showPassword():
+    global password
+    a = check_var.get()
+    password = entry2.get()
+    if a == 1: #selected 
+        password = password 
+    else: 
+        for i in range(len(password)):
+            hiddenpassword = ''.join(i = '*')
+            print(i)
+        password['text'] = hiddenpassword
+
+def login():
+    b = remember_var.get()
+    c = terms_var.get()
+    terms_bttn= terms.get()
+    username_entry = entry1.get()
+    password_entry = entry2.get()
+
+    if b == 1:
+        info = username_entry + '' + password_entry
+        f = open('userinfo.txt', 'w')
+        f.write(info)
+        f.close()
+        print(info)
+    if c == 1:
+        print('successfully logged in!')
+    if c== 0:
+        terms_bttn['fg'] = 'red'
+
+
+    
+
 
 root.geometry('300x200')
 frame1 = Frame(root, width = 200, height = 50)
@@ -90,25 +184,37 @@ frame3 = Frame(root, width = 200, height = 50)
 frame3.pack(fill = BOTH)
 frame4 = Frame(root, width = 200, height = 50)
 frame4.pack(fill = BOTH)
+frame5 = Frame(root, width = 200, height = 50)
+frame5.pack(fill = BOTH)
+frame6 = Frame(root, width = 200, height = 50)
+frame6.pack(fill = BOTH)
 
-password = 'password'
-label1 = Label(frame1, text= 'Generated Password: ',fg = 'blue').grid(row = 1, column = 1)
-label2 = Label(frame1, text = password, fg = 'black').grid(row = 1, column = 2)
+label1 = Label(frame1, text= 'Username: ',fg = 'black').grid(row = 1, column = 1)
+label2 = Label(frame1, fg = 'black')
+label2.grid(row = 1, column = 2)
 
-label3 = Label(frame2, text= 'Password Strength: ').grid(row = 2, column = 1)
-#three radios
-radio_variable = IntVar()
-radio_low = Radiobutton(frame2, text = 'Low', variable = radio_variable, value= 0)
-radio_low.grid(row =2, column = 2)
-radio_mid = Radiobutton(frame2, text = 'Medium', variable = radio_variable, value= 1)
-radio_mid.grid(row = 3, column = 2)
-radio_high = Radiobutton(frame2, text = 'High', variable = radio_variable, value= 2)
-radio_high.grid(row = 4, column = 2)
+entry1 = Entry(frame1)
+entry1.grid(row = 1, column = 2)
 
-label4 = Label(frame3, text= 'Password Length: ').grid(row = 5, column = 1)
-current_val = IntVar()
-password_length = Spinbox(frame3, from_ = 6, to= 12, textvariable = current_val).grid(row = 5, column = 2)
-submit_bttn = Button(frame4, text = 'Submit', command = submit, bg = 'green', fg = 'white').grid(row = 6, column = 2)
+label3 = Label(frame2, text= 'Password: ')
+label3.grid(row = 2, column = 1)
+entry2 = Entry(frame2)
+entry2.grid(row = 2, column = 2)
 
+check_var = IntVar()
+remember_var = IntVar()
+terms_var=  IntVar()
+check_password = Checkbutton(frame3, text= 'Show Password', variable = check_var, command = showPassword)
+check_password.grid(row = 3, column = 2)
+
+remember_password = Checkbutton(frame4, text= 'Remember Password', fg = 'blue', variable = remember_var)
+remember_password.grid(row = 4, column = 1)
+
+terms = Checkbutton(frame5, text= 'I accept the terms and conditions', fg = 'blue', variable = terms_var)
+terms.grid(row = 5, column = 1)
+
+login_bttn = Button(frame6, text = 'Login', fg = 'black', bg = 'gray', width = 15, command = login)
+login_bttn.grid(row = 6, column = 1, )
 
 root.mainloop()
+
