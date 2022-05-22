b675a235ea20b6ba9770b8f748b2122d2ab50452
Nayeli Pleskac <pleskac510@gmail.com>
2022-05-22 11:02:38 -0700

    file organization

--- set 5/calculator-project.py
+++ set 5/calculator-project.py
@@ -3,8 +3,30 @@ from tkinter import *
 from tkinter import messagebox
 
 from tkinter import filedialog as fd
-import wikipedia
+
 root = Tk()
-root.title('Custom Notebook App')
+root.title('Calculator Project')
 
 root.geometry('400x400')
+
+class Custom_button:
+    def __init__(self):
+        
+        # self.button = Button(root, text = num)
+        self.name = ['1','2','3','4','5','6','7','8','9']
+        self.row_number = 0
+        self.column_number = 0
+    def draw_button(self):
+        for name in range(0,10,1):
+            for row in range(0, 3, 1):
+                for column in range(0,3,1):
+                    # for num in range(0,10,1):
+                    num=row+column
+                    print('row ', row, 'column ', column, 'num ', name)
+                    button = Button(root, text = name, height = 3, width=5)  
+                    button.grid(row = row, column = column)
+
+
+buttonUI = Custom_button()
+buttonUI.draw_button()
+root.mainloop()
